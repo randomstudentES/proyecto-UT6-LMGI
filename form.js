363ec777492930ef6feb475e864275565e49773d
javier <jlirascand1@educacion.navarra.es>
2025-02-20 19:39:29 +0100

    19:06-19:39

--- form.js
+++ form.js
@@ -3,5 +3,17 @@ const emailValResult = document.getElementById("resultEmail");
 
 function comprobarCorreo(){
     event.preventDefault()
-    emailValResult.innerHTML = emailInput.value;
+    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]+$/;
+    if (regex.test(emailInput.value)){
+        emailValResult.style.color = "green";
+        emailValResult.innerHTML = `${emailInput.value} is valid :)`;
+    } else{
+        emailValResult.style.color = "red";
+        emailValResult.innerHTML = `${emailInput.value} is not correct, please retry :(`;
+    }
+}
+
+function activar(){
+    event.preventDefault();
+    location.href = "menu.html"
 }
